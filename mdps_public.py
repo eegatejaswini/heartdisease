a2cd9518e41f0bb57db56d54a7cbc43c7b78cec3
eegatejaswini <144467629+eegatejaswini@users.noreply.github.com>
2023-10-06 16:38:18 +0530

    mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -18,7 +18,7 @@ heart_disease_model = pickle.load(open('heart_disease_model.sav', 'rb'))
 # sidebar for navigation
 with st.sidebar:
     
-    selected = option_menu('Multiple Disease Prediction System',
+    selected = option_menu('HEART DISEASE PREDICTION SYSTEM',
                           
                           
                           ['Heart Disease Prediction'],
